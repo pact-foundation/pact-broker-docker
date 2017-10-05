7e472b3192396c2102a7c817b6416f722cfcf032
Beth Skurrie <beth@bethesque.com>
2017-10-06 10:15:18 +1100

    chore: check for TAG env var when releasing

--- script/release.sh
+++ script/release.sh
@@ -1,4 +1,6 @@
 DOCKER_IMAGE="dius/pact-broker"
 
+[[ -z "${TAG}" ]] && echo "Please set TAG environment variable" && exit 1
+
 echo "Tagging version ${TAG}"
 git tag -a "${TAG}" -m "Releasing version ${TAG}" && git push origin "${TAG}"

1dd63f514209a0221d3cb67bb66104da8dc4211c
Beth Skurrie <beth@bethesque.com>
2017-10-31 10:14:29 +1100

    chore: improve logic for getting pact_broker gem version

--- script/next-docker-tag.sh
+++ script/next-docker-tag.sh
@@ -1,5 +1,5 @@
 DOCKER_IMAGE=dius/pact-broker
-gem_version=$(cat pact_broker/Gemfile.lock | grep  "pact_broker" | head -1 | egrep -o "[0-9][0-9\.]+[0-9]")
+gem_version=$(BUNDLE_GEMFILE=pact_broker/Gemfile bundle exec ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")
 existing_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${DOCKER_IMAGE}/tags -O - | jq -r .[].name)
 existing_release_numbers_for_current_gem_version=$(echo "$existing_tags" | grep "${gem_version}-" | sed 's/'${gem_version}'-//g')
 last_release_number=$(printf "0\n${existing_release_numbers_for_current_gem_version}" | sort -g | tail -1)

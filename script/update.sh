166d555219962f1818f3aef3c3c855507756874b
Beth Skurrie <beth@bethesque.com>
2019-12-05 16:42:48 +1100

    chore: use pact broker version from built container

--- script/update.sh
+++ script/update.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+source script/docker-functions
+
 rm -rf tmp
-docker build . -f Dockerfile-bundle-base -t pact_broker_bundle_base
+docker_build_bundle_base
 docker run --rm -v ${PWD}/tmp:/tmp/bundle_update pact_broker_bundle_base:latest sh -c "bundle update && cp Gemfile.lock /tmp/bundle_update"
 mv tmp/Gemfile.lock pact_broker/
 
@@ -13,5 +15,5 @@ unset PACT_BROKER_DATABASE_PASSWORD
 PACT_BROKER_DATABASE_NAME=pact_broker.sqlite PACT_BROKER_DATABASE_ADAPTER=sqlite script/test.sh
 PACT_BROKER_DATABASE_NAME=pact_broker.sqlite PACT_BROKER_DATABASE_ADAPTER=sqlite script/test_basic_auth.sh
 git add pact_broker
-git commit -m "feat(gems): update pact_broker gem to version $(cd pact_broker && ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")"
+git commit -m "feat(gems): update pact_broker gem to version $(get_pact_broker_version)"
 git push

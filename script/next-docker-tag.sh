7d1271ddcfa2e24ed504ecfb77acb92bcb02acb8
Beth Skurrie <beth@bethesque.com>
2019-12-05 16:28:10 +1100

    chore: don't use bundle exec as the Gemfile.lock may be out of sync with the installed gems now, as it is updated in a docker environment

--- script/next-docker-tag.sh
+++ script/next-docker-tag.sh
@@ -1,5 +1,5 @@
 DOCKER_IMAGE="pactfoundation/pact-broker"
-gem_version=$(BUNDLE_GEMFILE=pact_broker/Gemfile bundle exec ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")
+gem_version=$(ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")
 existing_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${DOCKER_IMAGE}/tags -O - | jq -r .[].name)
 existing_release_numbers_for_current_gem_version=$(echo "$existing_tags" | grep "${gem_version}-" | sed 's/'${gem_version}'-//g')
 last_release_number=$(printf "0\n${existing_release_numbers_for_current_gem_version}" | sort -g | tail -1)

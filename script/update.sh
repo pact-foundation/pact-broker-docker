5ddfcb2c23c4b014750471ff461db1c0d5718407
Beth Skurrie <beth@bethesque.com>
2019-12-05 16:26:10 +1100

    chore: don't use bundle exec as the Gemfile.lock may be out of sync with the installed gems now, as it is updated in a docker environment

--- script/update.sh
+++ script/update.sh
@@ -13,5 +13,5 @@ unset PACT_BROKER_DATABASE_PASSWORD
 PACT_BROKER_DATABASE_NAME=pact_broker.sqlite PACT_BROKER_DATABASE_ADAPTER=sqlite script/test.sh
 PACT_BROKER_DATABASE_NAME=pact_broker.sqlite PACT_BROKER_DATABASE_ADAPTER=sqlite script/test_basic_auth.sh
 git add pact_broker
-git commit -m "feat(gems): update pact_broker gem to version $(cd pact_broker && bundle exec ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")"
+git commit -m "feat(gems): update pact_broker gem to version $(cd pact_broker && ruby -e "require 'pact_broker/version'; puts PactBroker::VERSION")"
 git push

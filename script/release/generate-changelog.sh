255c7ead302f6ca747161451ff6d9df1619b4d4a
Beth Skurrie <beth@bethesque.com>
2020-01-30 10:37:27 +1100

    chore: remove dirty hack for generating changelog
    [ci-skip]

--- script/release/generate-changelog.sh
+++ script/release/generate-changelog.sh
@@ -8,11 +8,8 @@ source script/docker-functions
 
 docker_build_package_base
 
-$(dirname "$0")/prepare-git-log-extract-for-conventional-changelog.sh
-
 docker run --rm -it \
   -e TAG=${TAG} \
-  -v ${PWD}/tmp/git-log:/tmp/git-log \
-  -v ${PWD}/CHANGELOG.md:/app/CHANGELOG.md \
+  -v ${PWD}:/app \
   pact_broker_package_base \
   sh -c "bundle exec rake generate_changelog"

291b0614fe6996d8bca945dd2441c51b75505348
Beth Skurrie <beth@bethesque.com>
2020-02-04 09:54:38 +1100

    chore: skip CI for changelog updates [ci-skip]

--- script/release.sh
+++ script/release.sh
@@ -5,7 +5,8 @@ set -e
 export TAG=$(script/release/next-docker-tag.sh)
 echo "Releasing tag ${TAG}"
 script/release/generate-changelog.sh
-git add CHANGELOG.md && git commit -m "chore(changelog): update for ${TAG}"
+git add CHANGELOG.md && git commit -m "chore(changelog): update for ${TAG}
+[ci-skip]"
 echo "Tagging version ${TAG}"
 git tag -a "${TAG}" -m "Releasing version ${TAG}" && git push origin "${TAG}"
 echo "Releasing from https://hub.docker.com/repository/docker/pactfoundation/pact-broker/builds"

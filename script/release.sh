67bd275172dcfa20747bc8b35df64aa2d302ea10
Beth Skurrie <beth@bethesque.com>
2020-02-13 13:17:50 +1100

    chore: push to master as well as the tag

--- script/release.sh
+++ script/release.sh
@@ -8,5 +8,7 @@ script/release/generate-changelog.sh
 git add CHANGELOG.md && git commit -m "chore(changelog): update for ${TAG}
 [ci-skip]"
 echo "Tagging version ${TAG}"
-git tag -a "${TAG}" -m "Releasing version ${TAG}" && git push origin "${TAG}"
+git tag -a "${TAG}" -m "Releasing version ${TAG}"
+git push origin "${TAG}"
+git push origin master
 echo "Releasing from https://hub.docker.com/repository/docker/pactfoundation/pact-broker/builds"

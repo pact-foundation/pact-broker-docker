bf006c70166583b4f472e1328201935fbb5d2a17
Beth Skurrie <beth@bethesque.com>
2020-05-07 08:55:21 +1000

    feat: allow Pact Broker to run on Heroku

--- pact_broker/database_connection.rb
+++ pact_broker/database_connection.rb
@@ -1,24 +1,41 @@
 require 'sequel'
 require_relative 'database_logger'
 
+DB_ENV_VAR_NAME = ENV.fetch('PACT_BROKER_DATABASE_URL_ENVIRONMENT_VARIABLE_NAME', 'PACT_BROKER_DATABASE_URL')
+URL = ENV[DB_ENV_VAR_NAME]
+SSL_MODE = ENV.fetch('PACT_BROKER_DATABASE_SSLMODE','')
+ADAPTER = ENV.fetch('PACT_BROKER_DATABASE_ADAPTER','') != '' ? ENV['PACT_BROKER_DATABASE_ADAPTER'] : 'postgres'
+USERNAME = ENV['PACT_BROKER_DATABASE_USERNAME']
+PASSWORD = ENV['PACT_BROKER_DATABASE_PASSWORD']
+HOST = ENV['PACT_BROKER_DATABASE_HOST']
+NAME = ENV['PACT_BROKER_DATABASE_NAME']
+PORT = ENV['PACT_BROKER_DATABASE_PORT']
+
 def create_database_connection(logger)
-  database_adapter = ENV.fetch('PACT_BROKER_DATABASE_ADAPTER','') != '' ? ENV['PACT_BROKER_DATABASE_ADAPTER'] : 'postgres'
-
-  config = {
-    adapter: database_adapter,
-    user: ENV['PACT_BROKER_DATABASE_USERNAME'],
-    password: ENV['PACT_BROKER_DATABASE_PASSWORD'],
-    host: ENV['PACT_BROKER_DATABASE_HOST'],
-    database: ENV['PACT_BROKER_DATABASE_NAME'],
-    encoding: 'utf8'
+  non_credential_options = {
+    encoding: 'utf8',
+    logger: DatabaseLogger.new(logger)
   }
+  non_credential_options[:sslmode] = SSL_MODE if SSL_MODE != ''
 
-  if ENV.fetch('PACT_BROKER_DATABASE_SSLMODE','') != ''
-    config[:sslmode] = ENV['PACT_BROKER_DATABASE_SSLMODE']
-  end
+  connection = if URL
+    uri = URI(URL)
+    uri.password = "*****"
+    logger.info "Connecting to database #{uri} with config: #{non_credential_options}"
+    Sequel.connect(URL, non_credential_options)
+  else
+    config = {
+      adapter: ADAPTER,
+      user: USERNAME,
+      password: PASSWORD,
+      host: HOST,
+      database: NAME,
+    }.merge(non_credential_options)
 
-  if ENV['PACT_BROKER_DATABASE_PORT'] =~ /^\d+$/
-    config[:port] = ENV['PACT_BROKER_DATABASE_PORT'].to_i
+    config[:port] = PORT.to_i if PORT =~ /^\d+$/
+
+    logger.info "Connecting to database with config: #{config.merge(password: "*****")}"
+    Sequel.connect(config)
   end
 
   ##
@@ -38,9 +55,7 @@ def create_database_connection(logger)
   # -1 means that connections will be validated every time, which avoids errors
   # when databases are restarted and connections are killed.  This has a performance
   # penalty, so consider increasing this timeout if building a frequently accessed service.
-  logger.info "Connecting to database with config: #{config.merge(password: "*****")}"
-  config[:logger] = DatabaseLogger.new(logger)
-  connection = Sequel.connect(config)
+
   connection.extension(:connection_validator)
   connection.pool.connection_validation_timeout = -1
   connection

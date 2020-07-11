7e3ec4451f902d81e68f2fae8f3bd100681b88b1
Beth Skurrie <beth@bethesque.com>
2020-07-11 19:09:30 +1000

    chore: update name of test service to match dockerhub's expectations

--- script/test.sh
+++ script/test.sh
@@ -11,16 +11,16 @@ trap cleanup EXIT
 cleanup
 
 docker build -t pactfoundation/pact_broker:localtest .
-docker-compose -f docker-compose-test.yml up --build --abort-on-container-exit --exit-code-from test
+docker-compose -f docker-compose-test.yml up --build --abort-on-container-exit --exit-code-from sut
 cleanup
 
 export PACT_BROKER_BASIC_AUTH_USERNAME=foo
 export PACT_BROKER_BASIC_AUTH_PASSWORD=bar
 export PACT_BROKER_PUBLIC_HEARTBEAT=true
-docker-compose -f docker-compose-test.yml up --build --abort-on-container-exit --exit-code-from test
+docker-compose -f docker-compose-test.yml up --build --abort-on-container-exit --exit-code-from sut
 
 unset PACT_BROKER_BASIC_AUTH_USERNAME
 unset PACT_BROKER_BASIC_AUTH_PASSWORD
 unset PACT_BROKER_PUBLIC_HEARTBEAT
 
-docker-compose -f docker-compose-test-different-env-var-names.yml up --build --abort-on-container-exit --exit-code-from test
+docker-compose -f docker-compose-test-different-env-var-names.yml up --build --abort-on-container-exit --exit-code-from sut

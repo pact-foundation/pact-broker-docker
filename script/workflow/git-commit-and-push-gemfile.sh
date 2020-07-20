04c3a131782b9ee1fa0122421322651a08ea5e86
Beth Skurrie <beth@bethesque.com>
2020-07-20 15:04:25 +1000

    chore: update release workflow

--- script/workflow/git-commit-and-push-gemfile.sh
+++ script/workflow/git-commit-and-push-gemfile.sh
@@ -5,10 +5,16 @@ set -euo
 git add pact_broker/Gemfile.lock
 
 if [ -n "${RELEASED_GEM_NAME}" ] && [ -n "${RELEASED_GEM_VERSION}" ]; then
-  git commit -m "feat(deps): update ${RELEASED_GEM_NAME} gem to version ${RELEASED_GEM_VERSION}"
+  git commit -m "feat(deps): update ${RELEASED_GEM_NAME} gem to version ${RELEASED_GEM_VERSION}
+
+[ci-skip]
+"
 else
   updated_gems=$(git diff --staged pact_broker/Gemfile.lock | grep '^+' | grep '(' | sed -e "s/+ *//" | paste -sd "," - | sed -e 's/,/, /g')
-  git commit -m "feat(deps): update to ${updated_gems}"
+  git commit -m "feat(deps): update to ${updated_gems}
+
+[ci-skip]
+"
 fi
 
 git push

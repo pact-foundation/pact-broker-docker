60582954ab2935c9315f0d2f050a61d784f98524
Beth Skurrie <beth@bethesque.com>
2020-07-24 16:47:10 +1000

    chore: update release scripts

--- script/release-workflow/run.sh
+++ script/release-workflow/run.sh
@@ -2,19 +2,21 @@
 
 set -euo >/dev/null
 
+cd "$(dirname "$0")"
+
 if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
-  script/workflow/git-configure.sh
-  script/workflow/docker-login.sh
+  ./git-configure.sh
+  ./docker-login.sh
 fi
 
-source script/workflow/set-env-vars.sh
-script/workflow/validate.sh
-script/workflow/docker-build.sh
+source ./set-env-vars.sh
+./validate.sh
+./docker-build.sh
 
 if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
   bundle install
 fi
 
-script/workflow/prepare-release.sh
-script/workflow/docker-push.sh
-script/workflow/git-push.sh
+./prepare-release.sh
+./docker-push.sh
+./git-push.sh

d936246334b1534b1e633b07d4be6b7b2e15a698
Beth Skurrie <beth@bethesque.com>
2020-07-24 16:28:04 +1000

    chore: force generation of changelog

--- script/workflow/prepare-release.sh
+++ script/workflow/prepare-release.sh
@@ -4,7 +4,7 @@ set -euo
 
 : "${TAG:?Please set the TAG environment variable}"
 
-bundle exec conventional-changelog version=${TAG}
+bundle exec conventional-changelog version=${TAG} force=true
 git add CHANGELOG.md
 git commit -m "chore(release): version ${TAG}"
 

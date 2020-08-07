f0ae3459a2949234506f078c4df82e59434a5198
Beth Skurrie <beth@bethesque.com>
2020-08-07 14:08:04 +1000

    chore: update tags in docker-compose files on release

--- script/release-workflow/prepare-release.sh
+++ script/release-workflow/prepare-release.sh
@@ -2,6 +2,14 @@
 
 set -euo >/dev/null
 
+docker_compose_files=$(find . -name "docker-compose*.yml" -not -name "*test*")
+
+for file in $docker_compose_files; do
+  cat $file | sed -e "s~image: pactfoundation/pact-broker:.*~image: pactfoundation/pact-broker:${TAG}~g" > dc-tmp
+  mv dc-tmp $file
+done
+
 bundle exec conventional-changelog version=${TAG} force=true
 git add CHANGELOG.md
+git add docker-compose*
 git commit -m "chore(release): version ${TAG}"

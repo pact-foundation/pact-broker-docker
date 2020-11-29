0414ac89f35d0a17481df2bd6fde9834fbec38b7
Beth Skurrie <beth@bethesque.com>
2020-11-30 10:13:12 +1100

    chore: update docker-compose-dev

--- script/docker/docker-compose-entrypoint.sh
+++ script/docker/docker-compose-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Sleeping 5 seconds to ensure the postgres database is up and running. TODO: change this to poll"
-sleep 5
+echo "Sleeping 10 seconds to ensure the postgres database is up and running. TODO: change this to poll"
+sleep 10
 /pact_broker/entrypoint.sh

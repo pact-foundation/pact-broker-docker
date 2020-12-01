aa69a9a62e42ae3d244d1cf67f2535973782a9c7
Beth Skurrie <beth@bethesque.com>
2020-12-01 20:18:04 +1100

    chore: fix bash

--- docker/postgres-entrypoint.sh
+++ docker/postgres-entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
-
+​
 dump=/tmp/pg-dump.sql
-
+​
 if [ -f "${dump}" ]; then
-  echo "Restoring dump from "${dump}""
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
       CREATE role pact_broker;
       GRANT ALL PRIVILEGES ON DATABASE postgres TO pact_broker;
-  EOSQL
-
+EOSQL
+​
   pg_restore --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --clean --if-exists --no-privileges "${dump}"
 else
   echo "Not restoring any dump file"

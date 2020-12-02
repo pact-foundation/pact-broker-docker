122c56c9ed2e474268d2f1e91d156e2f24a2f962
Beth Skurrie <beth@bethesque.com>
2020-12-02 18:44:29 +1100

    chore: remove dodgey characters [ci-skip]

--- docker/postgres-entrypoint.sh
+++ docker/postgres-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -e
-​
-dump=/tmp/pg-dump.sql
-​
+set -ex
+
+dump="/tmp/pg-dump.sql"
+
 if [ -f "${dump}" ]; then
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
       CREATE role pact_broker;
       GRANT ALL PRIVILEGES ON DATABASE postgres TO pact_broker;
 EOSQL
-​
+
   pg_restore --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --clean --if-exists --no-privileges "${dump}"
 else
   echo "Not restoring any dump file"

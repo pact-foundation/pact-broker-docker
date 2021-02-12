843a3a3f52e5ef7e0c136925043a4af7e704a412
Beth Skurrie <beth@bethesque.com>
2021-02-12 11:37:03 +1100

    chore: update trivy command

--- script/scan-inside-docker-container.sh
+++ script/scan-inside-docker-container.sh
@@ -2,4 +2,4 @@
 set -eu
 
 wget -q -O - https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh -s -- -b /usr/local/bin
-trivy filesystem --exit-code 1 --quiet /
+trivy filesystem --exit-code 1 --no-progress /

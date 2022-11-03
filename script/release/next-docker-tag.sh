4f894f0d17043767f9228462eee4c40bd6e31fb3
Beth Skurrie <beth.skurrie@smartbear.com>
2022-11-03 13:30:53 +1100

    chore: allow manual release through Github Actions UI

--- script/release/next-docker-tag.sh
+++ script/release/next-docker-tag.sh
@@ -6,6 +6,7 @@ set -e
 
 DOCKER_IMAGE="pactfoundation/pact-broker"
 gem_version=$(gem_version_from_gemfile_lock)
+# this is returning 410 Gone now - might need authentication. Means the next_release_number is always set to 0.
 existing_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${DOCKER_IMAGE}/tags -O - | jq -r .[].name | sed 's/-[0-9]$/.1/g')
 existing_release_numbers_for_current_gem_version=$(echo "$existing_tags" | grep "${gem_version}\." | sed 's/'${gem_version}'\.//g' | grep -E "^[0-9]+$" | cat)
 

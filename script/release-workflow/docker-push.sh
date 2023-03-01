fcee8eb02784b43e8680af3e7df6d867a323c265
Beth Skurrie <beth.skurrie@smartbear.com>
2023-03-02 10:48:55 +1100

    chore: fix docker push

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -5,8 +5,11 @@ set -euo >/dev/null
 if [ -n "${MAJOR_TAG}" ]; then
   docker tag ${DOCKER_IMAGE_ORG_AND_NAME}:latest ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
   docker push ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
-  docker push ${DOCKER_IMAGE_ORG_AND_NAME}:latest
 fi
 
 docker tag ${DOCKER_IMAGE_ORG_AND_NAME}:latest ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG}
 docker push ${DOCKER_IMAGE_ORG_AND_NAME}:${TAG}
+
+if [ "${PUSH_TO_LATEST}" != "false" ]; then
+  docker push ${DOCKER_IMAGE_ORG_AND_NAME}:latest
+fi
--- script/release-workflow/set-env-vars.sh
+++ script/release-workflow/set-env-vars.sh
@@ -4,7 +4,7 @@ export DOCKER_IMAGE_ORG_AND_NAME=pactfoundation/pact-broker
 
 if [ -n "${CUSTOM_TAG:-}" ]; then
   export TAG=$CUSTOM_TAG
-  export MAJOR_TAG=""
+  export MAJOR_TAG=$(echo $TAG | cut -d'.' -f1)
 else
   export TAG=$(script/release/next-docker-tag.sh)
   export MAJOR_TAG=$(echo $TAG | cut -d'.' -f1)

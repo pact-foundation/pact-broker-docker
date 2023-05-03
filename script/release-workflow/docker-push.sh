b6a1214ea458c564d543e0d2fb513c12bdb109ac
Beth Skurrie <beth.skurrie@smartbear.com>
2023-05-03 12:05:17 +1000

    chore: allow MAJOR_TAG to not be set

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -2,7 +2,7 @@
 
 set -euo >/dev/null
 
-if [ -n "${MAJOR_TAG}" ]; then
+if [ -n "${MAJOR_TAG:-}" ]; then
   docker tag ${DOCKER_IMAGE_ORG_AND_NAME}:latest ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
   docker push ${DOCKER_IMAGE_ORG_AND_NAME}:${MAJOR_TAG}
 fi

5355544fe684e4147ecc4d1ac3d3b90a4a24b9b6
Carlos Atkinson <carlos.atkinson@toptal.com>
2023-08-07 11:29:30 -0300

    Add puma-metrics

--- pact_broker/config/puma.rb
+++ pact_broker/config/puma.rb
@@ -1,6 +1,11 @@
 require_relative "../docker_configuration"
 
+plugin 'metrics'
+puma_metrics_port = ENV['PACT_BROKER_PUMA_METRICS_PORT'] || (PactBroker.docker_configuration.port.to_i + 1).to_s
+
 port PactBroker.docker_configuration.port
+
+metrics_url "tcp://0.0.0.0:#{puma_metrics_port}"
 
 if PactBroker.docker_configuration.puma_persistent_timeout
   persistent_timeout PactBroker.docker_configuration.puma_persistent_timeout

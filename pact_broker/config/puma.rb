992aa5969b712364f85a90b585fa6611abcb4c92
Beth Skurrie <bethesque@users.noreply.github.com>
2024-02-23 15:15:27 +1100

    feat: bind to IPv6 'any' as well as IPv4 'any' (#170)

--- pact_broker/config/puma.rb
+++ pact_broker/config/puma.rb
@@ -1,6 +1,7 @@
 require_relative "../docker_configuration"
 
-port PactBroker.docker_configuration.port
+bind "tcp://0.0.0.0:#{PactBroker.docker_configuration.port}"
+bind "tcp://[::]:#{PactBroker.docker_configuration.port}"
 
 if PactBroker.docker_configuration.puma_persistent_timeout
   persistent_timeout PactBroker.docker_configuration.puma_persistent_timeout

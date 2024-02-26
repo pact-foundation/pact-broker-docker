f0acc310b8531c4e7eb332d5a346f2ce9ffdad2f
Yousaf Nabi <you@saf.dev>
2024-02-26 17:00:01 +0000

    fix(ci): ghcr workflow POSIX sh, string replacement is undefined
    https://www.shellcheck.net/wiki/SC3060

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -13,7 +13,7 @@ push() {
 push_ghcr() {
   docker buildx build --platform=linux/amd64,linux/arm64,linux/arm \
   --output=type=image,push=true \
-  -t ghcr.io/${DOCKER_IMAGE_ORG_AND_NAME/pactfoundation/pact-foundation}:$1 .
+  -t ghcr.io/$(echo $DOCKER_IMAGE_ORG_AND_NAME | sed 's/pactfoundation/pact-foundation/g'):$1 .
 }
 
 if [ -n "${MAJOR_TAG:-}" ]; then

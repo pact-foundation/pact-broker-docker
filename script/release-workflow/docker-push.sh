8457eb0a09ed1c368ab5b4d7359ec06773f035d7
Yousaf Nabi <you@saf.dev>
2024-02-26 15:04:58 +0000

    fix: ghcr release workflow had missing arg

--- script/release-workflow/docker-push.sh
+++ script/release-workflow/docker-push.sh
@@ -13,7 +13,7 @@ push() {
 push_ghcr() {
   docker buildx build --platform=linux/amd64,linux/arm64,linux/arm \
   --output=type=image,push=true \
-  -t ghcr.io/${DOCKER_IMAGE_ORG_AND_NAME}:$1 
+  -t ghcr.io/${DOCKER_IMAGE_ORG_AND_NAME}:$1 .
 }
 
 if [ -n "${MAJOR_TAG:-}" ]; then

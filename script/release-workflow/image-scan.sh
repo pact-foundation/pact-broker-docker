4e133c7481c2cfd667623a73d29d2c878c372e42
Yousaf Nabi <yousaf@pactflow.io>
2026-07-20 15:16:44 +0100

    chore: cve exceptions for debian

--- script/release-workflow/image-scan.sh
+++ script/release-workflow/image-scan.sh
@@ -6,4 +6,4 @@ script_dir=$(cd "$(dirname $0)" && pwd)
 
 ARCH=${ARCH:-'amd64'}
 IS_DEBIAN=${IS_DEBIAN:+"-debian"}
-trivy image ${DOCKER_IMAGE_ORG_AND_NAME}:latest-${ARCH}${IS_DEBIAN} --exit-code 1 --ignorefile ${script_dir}/../.trivyignore
+trivy image ${DOCKER_IMAGE_ORG_AND_NAME}:latest-${ARCH}${IS_DEBIAN} --severity HIGH,CRITICAL --exit-code 1 --ignorefile ${script_dir}/../.trivyignore

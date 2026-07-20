585e2baa7e0dbae7760300c88b426f1dd9fbda35
Yousaf Nabi <yousaf@pactflow.io>
2026-07-20 12:01:06 +0100

    chore(debian): support -debian prefixed tags, audit, test, release

--- script/release-workflow/image-scan.sh
+++ script/release-workflow/image-scan.sh
@@ -5,4 +5,5 @@ set -euo >/dev/null
 script_dir=$(cd "$(dirname $0)" && pwd)
 
 ARCH=${ARCH:-'amd64'}
-trivy image ${DOCKER_IMAGE_ORG_AND_NAME}:latest-${ARCH} --exit-code 1 --ignorefile ${script_dir}/../.trivyignore
+IS_DEBIAN=${IS_DEBIAN:+"-debian"}
+trivy image ${DOCKER_IMAGE_ORG_AND_NAME}:latest-${ARCH}${IS_DEBIAN} --exit-code 1 --ignorefile ${script_dir}/../.trivyignore
